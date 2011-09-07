c685f3a94941d2074ab1ddff3c0cb71aaf456449
SigmaVirus24 <graffatcolmingov@gmail.com>
2011-09-07 18:22:23 -0400

    Change licensing area to docstring.
    This looks nicer if todo.py is loaded as a module in the python commandline as a
    module.

--- todo.py
+++ todo.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
-#### TODO.TXT-CLI-python
-#### Copyright (C) 2011  Sigmavirus24
-####
-#### This program is free software: you can redistribute it and/or modify
-#### it under the terms of the GNU General Public License as published by
-#### the Free Software Foundation, either version 3 of the License, or
-#### (at your option) any later version.
-####
-#### This program is distributed in the hope that it will be useful,
-#### but WITHOUT ANY WARRANTY; without even the implied warranty of
-#### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#### GNU General Public License for more details.
-####
-#### You should have received a copy of the GNU General Public License
-#### along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+TODO.TXT-CLI-python
+Copyright (C) 2011  Sigmavirus24
 
-#### TLDR: This is licensed under the GPLv3. See LICENSE for more details.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+TLDR: This is licensed under the GPLv3. See LICENSE for more details.
+"""
 
 import os
 import re
@@ -794,7 +796,7 @@ if __name__ == "__main__" :
 			"status"	: (False, _git_status),
 			"log"		: (False, _git_log),
 			}
-	commandsl = commands.keys()
+	commandsl = [intern(key) for key in commands.keys()]
 
 	if not len(args) > 0:
 		args.append(CONFIG["TODOTXT_DEFAULT_ACTION"])
