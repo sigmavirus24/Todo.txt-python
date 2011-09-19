8b2043f06aa5785a425483b7611350c742201fcf
SigmaVirus24 <graffatcolmingov@gmail.com>
2011-09-19 12:52:35 -0400

    Broken install.sh script.

--- install.sh
+++ install.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
-$INSTALL_DIR=$HOME/bin
-$BASH_ALIAS_FILE=$HOME/.bashrc
+INSTALL_DIR=$HOME/bin
+BASH_ALIAS_FILE=$HOME/.bashrc
+
+set -- $(getopt -l "help,install-dir::,alias-file::" "h" "$@")
+
+while [[ $# -gt 0 ]] ; do 
+	case "$1" in
+		"-h" | "--help" ) echo -e \
+			"Usage: $(basename $0) -h[--help] [--install-dir] [--alias-file]\n"
+			exit
+			;;
+		"--install-dir" ) shift; INSTALL_DIR=$1
+			;;
+		"--alias-file" ) shift; BASH_ALIAS_FILE=$1
+			;;
+		"--" ) break
+			;;
+		* ) shift
+	esac
+done
 
 [[ -d $INSTALL_DIR ]] || mkdir -p $INSTALL_DIR
+echo "$INSTALL_DIR check passed."
 
 [[ -s $BASH_ALIAS_FILE ]] || echo "# Bash RC File" >> $BASH_ALIAS_FILE
+echo "$BASH_ALIAS_FILE check passed."
 ## Believe it or not, >> is faster than >.
 
 echo "[install.sh] Copying todo.py to $INSTALL_DIR/todo.py"
-cp ./todo.py $INSTALL_DIR
+#cp ./todo.py $INSTALL_DIR
 
 ## Establish alias
 if grep -q "todo.sh" $BASH_ALIAS_FILE ; then
-	$ALIAS="\n\nalias tpy='$INSTALL_DIR/todo.py'\n"
+	ALIAS="\n\nalias tpy='$INSTALL_DIR/todo.py'\n"
 else
-	$ALIAS="\n\nalias t='$INSTALL_DIR/todo.py'\n"
+	ALIAS="\n\nalias t='$INSTALL_DIR/todo.py'\n"
 fi
 
-echo -e $ALIAS >> $BASH_ALIAS_FILE
+echo $ALIAS
+#echo -e $ALIAS >> $BASH_ALIAS_FILE
