fed9f7818948ba73ee1079f85a373b3b88aa2d0d
SigmaVirus24 <graffatcolmingov@gmail.com>
2011-12-15 08:59:26 -0500

    More tests to test_formatting.
    Issue #8

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -35,32 +35,64 @@ class TestFormat(base.BaseTest):
 
 	def test_formatted(self):
 		self.addm_todo_no_pri(self.num)
-		#self.assertNumLines(self.num, "Test\s\d+")
+		#lines = todo.format_lines()
+		#self.assert_formatted(lines)
+
+
+	def test_color_only(self):
+		self.addm_todo_with_pri(self.num)
+		lines = todo.format_lines(True)
+		self.assert_color_only(lines)
+
 
 	def test_formatted_remove_pri(self):
 		self.addm_todo_with_pri(self.num)
 		#self.assertNumLines(self.num, "Test \d+")
 
+
 	def addm_todo_no_pri(self, n):
 		todo.addm_todo("\n".join(self._test_lines_no_pri(n)))
+
 
 	def addm_todo_with_pri(self, n):
 		todo.addm_todo("\n".join(self._test_lines_pri(n)))
 
-	def assert_formatted(self):
+
+	def assert_formatted(self, lines):
 		# This needs to check that the string starts with the proper code (i.e.
 		# todo.TERM_COLORS["red"] and terminates with
 		# todo.TERM_COLORS["default"]\n for the proper colors based on priority
+		# also checks that the return value is a dictionary.
 		pass
+
+
+	def assert_color_only(self, lines):
+		# This needs to check that the return value is only a list of strings
+		# colored by priority.
+		self.assertTrue(isinstance(lines, list))
+		# Not finished, just testing the above statement.
+
 
 	def assert_plain(self):
 		# This needs to check that only the todo.TERM_COLORS["default"] color
-		# has been applied to the beginning and end of the string
+		# has been applied to the beginning and end of the string. Also needs to
+		# check that the return value is a dictionary
 		pass
+
 
 	def assert_nopri(self):
 		# This should check to make sure the function removes the priorities
 		# from the beginning of the lines (formatted or otherwise).
+		pass
+
+
+	def assert_dated(self):
+		# Should check that the formatting is in date form and properly sorted.
+		pass
+
+
+	def assert_context(self):
+		# Should check that the return value is sorted by context.
 		pass
 
 if __name__ == "__main__":
