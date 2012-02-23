d6b7044fa538e7bc205188cf414c623c7feca4e3
SigmaVirus24 <graffatcolmingov@gmail.com>
2012-02-23 14:56:51 -0500

    Upgrade make_download.sh

--- utils/make_download.sh
+++ utils/make_download.sh
@@ -2,5 +2,10 @@
 
 FILES=('todo.py' 'install.sh' 'README.md' 'LICENSE')
 
-tar czf todo.txt-python.tar.gz ${FILES[@]}
-zip -q todo.txt-python.zip ${FILES[@]}
+base_name="todo.txt-python"
+if [[ -n $1 ]] ; then
+    base_name=${base_name}"-"${1}
+fi
+
+tar czf ${base_name}.tar.gz ${FILES[@]}
+zip -q ${base_name}.zip ${FILES[@]}
