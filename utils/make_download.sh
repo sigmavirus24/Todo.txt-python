d31439458e3f9c351a62dace4a4b0bb7dd043f76
SigmaVirus24 <graffatcolmingov@gmail.com>
2012-02-23 20:03:45 -0500

    Upgrade make_download.sh some more.

--- utils/make_download.sh
+++ utils/make_download.sh
@@ -5,7 +5,14 @@ FILES=('todo.py' 'install.sh' 'README.md' 'LICENSE')
 base_name="todo.txt-python"
 if [[ -n $1 ]] ; then
     base_name=${base_name}"-"${1}
+else 
+    branch="$(git status | sed -rn '/On branch/ s/.*branch (.*)/\1/p')"
+    if [[ ${branch} != "master" ]] ; then
+        commit="$(git log --oneline | sed -nr '1 s/(.*) [A-Z].*/\1/p')"
+        base_name=${base_name}"-"${branch}"-"${commit}
+    fi
 fi
+
 
 tar czf ${base_name}.tar.gz ${FILES[@]}
 zip -q ${base_name}.zip ${FILES[@]}
