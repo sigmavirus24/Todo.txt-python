d39c2e075efdb5bb4a6f6d04ce35e39218cca158
SigmaVirus24 <graffatcolmingov@gmail.com>
2012-02-29 23:45:55 -0500

    Make it easier to visually grep the test output.

--- run_tests.py
+++ run_tests.py
@@ -37,4 +37,5 @@ if __name__ == "__main__":
         names = [ "".join(["tests.", a]) for a in tmp ]
         suite = unittest.defaultTestLoader.loadTestsFromNames(names)
 
+    print("=== Starting unittest suite ===")
     unittest.TextTestRunner(verbosity=2).run(suite)
