a5d3c0ef636c5753edf35dbf0a45420fa6291cde
SigmaVirus24 <graffatcolmingov@gmail.com>
2012-03-09 09:33:05 -0500

    Re-indent base.py. Try to fix travis problems.

--- tests/base.py
+++ tests/base.py
@@ -29,89 +29,93 @@ todotxt = todo.CONFIG["TODO_FILE"] = "test_todo.txt"
 donetxt = todo.CONFIG["DONE_FILE"] = "test_done.txt"
 
 class BaseTest(unittest.TestCase):
-	num = 50
+    num = 50
 
-	def setUp(self):
-		todo.CONFIG["PRE_DATE"] = False
-		todo.CONFIG["TODO_PY"] = "testing"
-		sys.stdout = open(os.devnull, 'w')
-		open(todotxt, "w+").close()
-		open(donetxt, "w+").close()
+    def default_config(self):
+        pass
 
-
-	def tearDown(self):
-		sys.stdout = sys.__stdout__
-		if os.path.isfile(todotxt):
-			os.unlink(todotxt)
-		if os.path.isfile(donetxt):
-			os.unlink(donetxt)
+    def setUp(self):
+        todo.CONFIG["PRE_DATE"] = False
+        todo.CONFIG["TODO_PY"] = "testing"
+        todo.default_config = self.default_config
+        sys.stdout = open(os.devnull, 'w')
+        open(todotxt, "w+").close()
+        open(donetxt, "w+").close()
 
 
-	def count_matches(self, regexp=None):
-		count = 0
-		for line in todo.iter_todos():
-			if regexp == None or re.match(regexp, line):
-				count += 1
-		return count
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+        if os.path.isfile(todotxt):
+            os.unlink(todotxt)
+        if os.path.isfile(donetxt):
+            os.unlink(donetxt)
 
 
-	def _test_lines_no_pri(self, num):
-		return ["Test {0}".format(i) for i in range(0, num)]
+    def count_matches(self, regexp=None):
+        count = 0
+        for line in todo.iter_todos():
+            if regexp == None or re.match(regexp, line):
+                count += 1
+        return count
 
 
-	def _test_lines_pri(self, num):
-		n = len(todo.PRIORITIES)
-		p = todo.PRIORITIES
-		return ["({0}) Test {1}".format(p[i % n], i) for i in range(0, num)]
+    def _test_lines_no_pri(self, num):
+        return ["Test {0}".format(i) for i in range(0, num)]
 
 
-	def _test_lines_date(self, num):
-		l = self._test_lines_pri(num)
-		m = []
-		start_date = datetime.date.today()
-
-		for d, l in zip((start_date + datetime.timedelta(n) for n in range(num)), l):
-			m.append(todo.concat([l, " #{%s}" % d.isoformat()]))
-		return m
+    def _test_lines_pri(self, num):
+        n = len(todo.PRIORITIES)
+        p = todo.PRIORITIES
+        return ["({0}) Test {1}".format(p[i % n], i) for i in range(0, num)]
 
 
-	def _test_lines_project(self, num):
-		projects = ["+foo", "+bar", "+bogus", "+github", "+school", "+work",
-				"+inthemorning", "+agenda", "+noagenda"]
-		n = len(projects)
-		l = self._test_lines_pri(num)
-		m = []
-		for i in range(0, num):
-			m.append(todo.concat([l[i], projects[i % n]], " "))
-		return m
+    def _test_lines_date(self, num):
+        l = self._test_lines_pri(num)
+        m = []
+        start_date = datetime.date.today()
+
+        for d, l in zip((start_date + datetime.timedelta(n) for n in range(num)), l):
+            m.append(todo.concat([l, " #{%s}" % d.isoformat()]))
+        return m
 
 
-	def _test_lines_context(self, num):
-		projects = ["@foo", "@bar", "@bogus", "@github", "@school", "@work",
-				"@inthemorning", "@agenda", "@noagenda"]
-		n = len(projects)
-		l = self._test_lines_pri(num)
-		m = []
-		for i in range(0, num):
-			m.append(todo.concat([l[i], projects[i % n]], " "))
-		return m
+    def _test_lines_project(self, num):
+        projects = ["+foo", "+bar", "+bogus", "+github", "+school", "+work",
+                "+inthemorning", "+agenda", "+noagenda"]
+        n = len(projects)
+        l = self._test_lines_pri(num)
+        m = []
+        for i in range(0, num):
+            m.append(todo.concat([l[i], projects[i % n]], " "))
+        return m
 
 
-	def assertNumLines(self, exp, regexp=None):
-		c = self.count_matches(regexp)
-		self.assertEqual(exp, c)
+    def _test_lines_context(self, num):
+        projects = ["@foo", "@bar", "@bogus", "@github", "@school", "@work",
+                "@inthemorning", "@agenda", "@noagenda"]
+        n = len(projects)
+        l = self._test_lines_pri(num)
+        m = []
+        for i in range(0, num):
+            m.append(todo.concat([l[i], projects[i % n]], " "))
+        return m
 
 
-	def assertIsInstance(self, obj, cls, msg=None):
-		if sys.version_info >= (2, 7):
-			super(BaseTest, self).assertIsInstance(obj, cls, msg)
-		else:
-			self.assertTrue(isinstance(obj, cls))
+    def assertNumLines(self, exp, regexp=None):
+        c = self.count_matches(regexp)
+        self.assertEqual(exp, c)
 
 
-	def assertIsNotNone(self, expr, msg=None):
-		if sys.version_info >= (2, 7):
-			super(BaseTest, self).assertIsNotNone(expr, msg)
-		else:
-			if not expr:
-				self.fail(msg)
+    def assertIsInstance(self, obj, cls, msg=None):
+        if sys.version_info >= (2, 7):
+            super(BaseTest, self).assertIsInstance(obj, cls, msg)
+        else:
+            self.assertTrue(isinstance(obj, cls))
+
+
+    def assertIsNotNone(self, expr, msg=None):
+        if sys.version_info >= (2, 7):
+            super(BaseTest, self).assertIsNotNone(expr, msg)
+        else:
+            if not expr:
+                self.fail(msg)
