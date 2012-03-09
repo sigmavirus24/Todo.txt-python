f49740ff33cd35eab92a6a318b0fa73b4fbf4348
SigmaVirus24 <graffatcolmingov@gmail.com>
2012-03-08 21:34:54 -0500

    Preliminary get_config() tests.

---
+++ tests/test_getconfig.py
@@ -0,0 +1,56 @@
+# TODO.TXT-CLI-python test script
+# Copyright (C) 2011-2012  Sigmavirus24
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# TLDR: This is licensed under the GPLv3. See LICENSE for more details.
+
+import todo
+import base
+import re
+from os import listdir
+from functools import partial
+
+class TestConfig(base.BaseTest):
+
+    def setUp(self):
+        file_re = re.compile('(\w+\/\w+\.)config')
+        # Just some nice magic:
+        self.sub = partial(file_re.sub, '\g<1>plain')
+        super(TestConfig, self).setUp()
+        self.backup = todo.CONFIG.copy()
+
+    def tearDown(self):
+        todo.CONFIG = self.backup
+        super(TestConfig, self).setUp()
+
+    def config_assert(self, key, val):
+        self.assertEquals(todo.CONFIG[key], val)
+
+    def _validate_(filename):
+        filename = self.sub(filename)
+        with open(filename, r) as fd:
+            for line in fd:
+                key, val, = line.split()
+                if val == "False":
+                    val = False
+                elif val == "True":
+                    val = True
+                self.config_assert(key, val)
+
+    def test_configs(self):
+        for f in listdir('tests/config/'):
+            if f.endswith('config'):
+                f = ''.join(['tests/config/', f])
+                todo.get_config(config_name=f)
